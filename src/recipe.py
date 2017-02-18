ef86bf97351bd10f379575dfb9cf6e69c5f65934
Isaac Park <isaac.a.park@gmail.com>
2017-02-18 03:29:11 -0500

    List Cake and Add Them

--- src/recipe.py
+++ src/recipe.py
@@ -14,8 +14,15 @@ class Recipe(BaseModel):
 
 class Pairing(BaseModel):
     why = CharField()
-    startingWith =  ForeignKeyField(Recipe, related_name='pairings')
-    endingWith =  ForeignKeyField(Recipe, related_name='pairingsResults')
+    starting_with =  ForeignKeyField(Recipe, related_name='pairings')
+    ending_with =  ForeignKeyField(Recipe, related_name='pairingsResults')
+    def get_dict(self):
+        return {
+            'starting_with': self.starting_with.get_dict(),
+            'ending_with': self.ending_with.get_dict(),
+            'why': self.why,
+            'id': self.id
+        }
 
 class Ingredient(BaseModel):
     name = CharField()
--- src/run.py
+++ src/run.py
@@ -1,24 +1,54 @@
 from flask import Flask, jsonify, request
 import flask_cors
 from flask_cors import CORS, cross_origin
-from recipe import Recipe
+from recipe import Recipe, Pairing
+from functools import partial
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/cake', methods=['POST'])
-def addCake():
+def addRecipies(category):
     json = request.get_json()
-    recipe = Recipe(name=json['name'], category=1)
+    recipe = Recipe(name=json['name'], category=category)
     recipe.save()
     return jsonify({'status': 'success', 'recipe': recipe.get_dict()})
 
+def getRecipies(category):
+    cakes = []
+    for cake in Recipe.select().where(Recipe.category==category):
+        cakes.append(cake.get_dict())
+    return jsonify({'status': 'success', 'recipes': cakes})
+
+
+@app.route('/cake', methods=['POST'])
+def addCake():
+    return addRecipies(1)
+
 @app.route('/cakes', methods=['get'])
 def getCakes():
-    cakes = []
-    for cake in Recipe.select().where(Recipe.category==1):
-        cakes.append(cake.get_dict())
-    return jsonify({'status': 'success', 'cake': cakes})
+    return getRecipies(1)
+
+
+@app.route('/icing', methods=['POST'])
+def addIcing():
+    return addRecipies(2)
+
+@app.route('/icings', methods=['get'])
+def getIcings():
+    return getRecipies(2)
+
+@app.route('/pairings')
+def getPairings():
+    pairings = []
+    for pairing in Pairing.select():
+        pairings.append(paring.get_dict())
+    return jsonify({'status': 'success', 'pairings': pairings})
+
+@app.route('/pairing', methods=['POST'])
+def addPairing():
+    json = request.get_json()
+    pairing = Pairing(why=[json['why'], starting_with=json['starting_with'])
+
 
 
 if __name__ == "__main__":
